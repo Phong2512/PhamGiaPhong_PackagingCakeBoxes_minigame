05edd0b154d304f0b88a1d2f6cf2afb05a968baf
Phong2512 <110968833+Phong2512@users.noreply.github.com>
2024-03-08 10:31:48 +0700

    FisrtPush'

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    //[SerializeField] GameObject box_Prefabs;
+    //[SerializeField] GameObject board_Obj;
+    //private int coutBoard = 9;
+    //private void Start()
+    //{
+    //    CreateBoard();
+    //}
+    //private void CreateBoard()
+    //{
+    //    for (int i = 0; i < coutBoard; i++)
+    //    {
+    //        GameObject newBox = Instantiate(box_Prefabs, board_Obj.transform.position, Quaternion.identity, board_Obj.transform);
+    //    }
+    //}
+}
---
+++ Assets/Scripts/TileState.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+
+[CreateAssetMenu(menuName = "Tile State")]
+public class TileState : ScriptableObject
+{
+    public Sprite tile_SPT;
+    public int tile_ID;
+    public float width;
+    public float height;
+}
